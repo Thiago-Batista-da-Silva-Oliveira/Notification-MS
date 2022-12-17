8ff1f2017884b0d332e672d57f6bced7df4c9be6
Thiago <gmoliveira69@gmail.com>
2022-12-17 14:12:02 -0300

    send notification

---
+++ src/use-cases/send-notification.ts
@@ -0,0 +1,30 @@
+import { Content } from 'src/application/entities/content';
+import { Notification } from 'src/application/entities/notification';
+
+interface SendNotificationRequest {
+  recipientId: string;
+  content: string;
+  category: string;
+}
+
+interface SendNotificationResponse {
+  notification: Notification;
+}
+
+export class SendNotification {
+  async execute(
+    request: SendNotificationRequest,
+  ): Promise<SendNotificationResponse> {
+    const { recipientId, content, category } = request;
+
+    const notification = new Notification({
+      recipientId,
+      category,
+      content: new Content(content),
+    });
+
+    return {
+      notification,
+    };
+  }
+}
