5abe361839d4feaf966d08d74b53453119d55b9c
Thiago <gmoliveira69@gmail.com>
2022-12-17 21:43:47 -0300

    inject repository

--- src/infra/database/prisma/repositories/prisma-notifications-repositories.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repositories.ts
@@ -1,7 +1,9 @@
+import { Injectable } from '@nestjs/common';
 import { Notification } from 'src/application/entities/notification';
 import { NotificationsRepository } from 'src/application/repositories/notification-repository';
 import { PrismaService } from '../prisma.service';
 
+@Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
   constructor(private prismaService: PrismaService) {}
   async create(notification: Notification): Promise<void> {
