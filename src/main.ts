9d5d009d66c1ff29b6281bfb14ca5055b583988b
Thiago <gmoliveira69@gmail.com>
2022-12-26 18:02:38 -0300

    listener

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,26 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+import { Transport } from '@nestjs/microservices';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.RMQ,
-      options: {
-        urls: ['amqp://localhost:5672'],
-        queue: 'notification',
-        queueOptions: {
-          durable: false,
-        },
+  const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'notification',
+      queueOptions: {
+        durable: false,
       },
     },
-  );
+  });
 
-  app.listen();
+  await app.startAllMicroservices();
+
+  await app.listen(3337, () => console.log('server started'));
 }
+
 bootstrap();
